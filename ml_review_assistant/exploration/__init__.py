51acfa83c0440cf08578d9433c02f96ee8504615
Arnour Sabino <arnour.sabino@gmail.com>
2021-05-25 14:40:21 -0300

    Add model resumer

---
+++ ml_review_assistant/exploration/__init__.py
@@ -0,0 +1,59 @@
+from gensim.corpora.dictionary import Dictionary
+from gensim.models.coherencemodel import CoherenceModel
+from operator import itemgetter
+from collections import Counter
+import numpy as np
+
+
+class Explorer:
+    """Expose topic modeling metrics"""
+
+    def __init__(self, tokens):
+        self.__tokens = tokens
+        self.__dictionary = Dictionary(tokens)
+        self.__corpus = [self.__dictionary.doc2bow(token) for token in tokens]
+        self.__scores = []
+        flatten_tokens = [item for sublist in tokens for item in sublist]
+        self.__top = Counter(flatten_tokens).most_common(100)
+
+    def scores(self):
+        return self.__scores.copy()
+
+    def top(self):
+        return self.__top.copy()
+
+    def best(self):
+        return sorted(self.__scores, key=itemgetter(1), reverse=True)[0][0]
+
+    def coherence(self, topics, model_builder, measure='u_mass'):
+        cm = CoherenceModel(
+            model=model_builder(topics, self.__corpus, self.__dictionary),
+            corpus=self.__corpus,
+            texts=self.__tokens,
+            dictionary=self.__dictionary,
+            coherence=measure
+        )
+        self.__scores.append((topics, round(cm.get_coherence(), 5)))
+
+    def resume(self, topics, model_builder):
+        model = model_builder(topics, self.__corpus, self.__dictionary)
+        topics_resume = []
+        for topic_id in range(model.num_topics):
+            keywords = [word for word, _ in model.show_topic(topic_id)]
+            topics_resume.append(keywords)
+        res = []
+        for token_index, row in enumerate(model[self.__corpus]):
+            row = sorted(row, key=itemgetter(1), reverse=True)
+            (topic_id, topic_score) = row[0]
+            res.append(
+                np.array(
+                    [
+                        int(topic_id),
+                        round(topic_score, 5),
+                        topics_resume[topic_id],
+                        token_index
+                    ],
+                    dtype=object
+                )
+            )
+        return res
---
+++ tests/test_exploration.py
@@ -0,0 +1,87 @@
+from . import TestBase
+from ml_review_assistant.exploration import Explorer
+from gensim.models.nmf import Nmf
+from gensim.test.utils import common_texts
+import numpy as np
+
+
+class ExplorationTestCase(TestBase):
+
+    def setUp(self):
+        self.tokens = common_texts
+        self.explorer = Explorer(tokens=self.tokens)
+
+    def test_generate_coherence_score_for_each_model(self):
+        for topics in range(5, 20, 5):
+            self.explorer.coherence(topics, self.__model_builder)
+
+        scores = self.explorer.scores()
+
+        self.assertEqual(len(scores), 3)
+        self.assertEqual(scores[0][0], 5)
+        self.assertIsNotNone(scores[0][1])
+
+        self.assertEqual(scores[1][0], 10)
+        self.assertIsNotNone(scores[0][1])
+
+        self.assertEqual(scores[2][0], 15)
+        self.assertIsNotNone(scores[0][1])
+
+    def test_select_best_number_of_topics(self):
+        for topics in range(1, 4):
+            self.explorer.coherence(topics, self.__model_builder)
+
+        self.assertEqual(self.explorer.best(), 2)
+
+    def test_select_top_tokens(self):
+        expected_top = [('system', 4),
+                        ('user', 3),
+                        ('trees', 3),
+                        ('graph', 3),
+                        ('human', 2),
+                        ('interface', 2),
+                        ('computer', 2),
+                        ('survey', 2),
+                        ('response', 2),
+                        ('time', 2),
+                        ('eps', 2),
+                        ('minors', 2)]
+
+        self.assertEqual(self.explorer.top(), expected_top)
+
+    def test_resume_topics(self):
+        result = self.explorer.resume(2, self.__model_builder)
+        topic0_keywords = [
+            'system', 'eps', 'user', 'human', 'interface',
+            'response', 'time', 'graph', 'computer', 'trees'
+        ]
+        topic1_keywords = [
+            'survey', 'computer', 'time', 'response', 'user',
+            'graph', 'minors', 'trees', 'system', 'interface'
+        ]
+
+        expected = [
+            np.array([1, 0.67387, topic1_keywords, 0], dtype='object'),
+            np.array([1, 0.79334, topic1_keywords, 1], dtype='object'),
+            np.array([0, 1.0, topic0_keywords, 2], dtype='object'),
+            np.array([0, 1.0, topic0_keywords, 3], dtype='object'),
+            np.array([1, 0.88351, topic1_keywords, 4], dtype='object'),
+            np.array([1, 1.0, topic1_keywords, 5], dtype='object'),
+            np.array([1, 1.0, topic1_keywords, 6], dtype='object'),
+            np.array([1, 1.0, topic1_keywords, 7], dtype='object'),
+            np.array([1, 1.0, topic1_keywords, 8], dtype='object'),
+        ]
+
+        for r, e in zip(result, expected):
+            self.assertEqual(r[0], e[0])
+            self.assertAlmostEqual(r[1], e[1])
+            self.assertEqual(r[2], e[2])
+            self.assertEqual(r[3], e[3])
+
+    def __model_builder(self, t, c, d):
+        return Nmf(
+            corpus=c,
+            id2word=d,
+            num_topics=t,
+            random_state=42,
+        )
